6d587fb902f1fe363cd9dcf427d40ee17735ed3b
Yttehs <shetty.yttehs@outlook.com>
2025-05-21 16:29:56 +0800

    chore: remove sample code

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,18 @@
 
 use embassy_executor::Spawner;
 use embassy_time::Timer;
-use ch32_hal::gpio::{AnyPin, Level, Output, Pin};
-use ch32_hal::println;
+use hal::println;
 use panic_halt as _;
+use ch32_hal as hal;
 
 const RAW_IMAGE: &[u8] = include_bytes!("../assets/ferris.raw");
 
-#[embassy_executor::task]
-async fn blink(pin: AnyPin, interval_ms: u64) {
-    let mut led = Output::new(pin, Level::Low, Default::default());
-
-    loop {
-        led.set_high();
-        Timer::after_millis(interval_ms).await;
-        led.set_low();
-        Timer::after_millis(interval_ms).await;
-    }
-}
-
 #[embassy_executor::main(entry = "qingke_rt::entry")]
-async fn main(spawner: Spawner) -> ! {
-    ch32_hal::debug::SDIPrint::enable();
-    let p = ch32_hal::init(ch32_hal::Config::default());
+async fn main(_spawner: Spawner) -> ! {
+    hal::debug::SDIPrint::enable();
+    let hal_config = hal::Config::default();
+    let p = hal::init(hal_config);
 
-    // Adjust the LED GPIO according to your board
-    spawner.spawn(blink(p.PA0.degrade(), 1000)).unwrap();
     loop {
         Timer::after_millis(1000).await;
         println!("tick");
