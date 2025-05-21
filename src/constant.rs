fb40dc317a1c378df0680f6f2e3f30b600c52041
Yttehs <shetty.yttehs@outlook.com>
2025-05-21 16:53:52 +0800

    feat: impl show image

--- src/constant.rs
+++ src/constant.rs
@@ -1,7 +1,7 @@
 // assets/ferris.raw image
 pub const RAW_IMAGE: &[u8] = include_bytes!("../assets/ferris.raw");
-pub const IMAGE_WIDTH: usize = 86;
-pub const IMAGE_HEIGHT: usize = 84;
+pub const IMAGE_WIDTH: u16 = 86;
+pub const IMAGE_HEIGHT: u16 = 84;
 
 // ST7735 display
 pub const ST7735_WIDTH: u16 = 144;
--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,23 @@
 #![feature(impl_trait_in_assoc_type)]
 
 use ch32_hal::{self as hal};
+use constant::{IMAGE_HEIGHT, IMAGE_WIDTH, RAW_IMAGE, ST7735_HEIGHT, ST7735_WIDTH};
 use embassy_executor::Spawner;
-use embassy_time::Timer;
+use embassy_time::{Delay, Timer};
+use embedded_graphics::{
+    draw_target::DrawTarget,
+    image::ImageDrawable,
+    pixelcolor::{raw::LittleEndian, Rgb565},
+    prelude::RgbColor,
+};
 use hal::{
     gpio::{Level, Output, Pin},
     println,
     spi::Spi,
 };
 
-mod lang_items;
 mod constant;
+mod lang_items;
 
 #[embassy_executor::main(entry = "qingke_rt::entry")]
 async fn main(_spawner: Spawner) -> ! {
@@ -26,6 +33,30 @@ async fn main(_spawner: Spawner) -> ! {
 
     let (sck, sda) = (p.PB3, p.PB5);
     let spi = Spi::new_blocking_txonly(p.SPI3, sck, sda, Default::default());
+    let spi_device = embedded_hal_bus::spi::ExclusiveDevice::new_no_delay(spi, cs).unwrap();
+
+    let mut display = st7735_lcd::ST7735::new(
+        spi_device,
+        dc,
+        res,
+        false,
+        false,
+        ST7735_WIDTH as u32,
+        ST7735_HEIGHT as u32,
+    );
+
+    display.init(&mut Delay).unwrap();
+    display
+        .set_orientation(&st7735_lcd::Orientation::Landscape)
+        .unwrap();
+    display.clear(Rgb565::BLACK).unwrap();
+    let xpos = (ST7735_WIDTH - IMAGE_WIDTH) / 2;
+    let ypos = (ST7735_HEIGHT - IMAGE_HEIGHT) / 2;
+    let image: embedded_graphics::image::ImageRaw<Rgb565, LittleEndian> =
+        embedded_graphics::image::ImageRaw::new(RAW_IMAGE, IMAGE_WIDTH as u32);
+
+    display.set_offset(xpos, ypos);
+    image.draw(&mut display).unwrap();
 
     loop {
         Timer::after_millis(1000).await;
